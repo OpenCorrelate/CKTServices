b884b4d8dd9469af45a3d939e8fdcece89eec46e
Prez Cannady <revprez@opencorrelate.org>
2017-09-10 12:42:24 -0400

    Experimenting.

---
+++ src/main/objc/macos/Trust_MacOS_OBJC/Trust_MacOS_OBJC/OC_TMO_Globals.h
@@ -0,0 +1,13 @@
+//
+//  OC_TMO_Globals.h
+//  Trust_MacOS_OBJC
+//
+//  Created by Presley Cannady on 9/10/17.
+//  Copyright © 2017 Open Correlate. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OC_TMO_Globals : NSObject
+
+@end
---
+++ src/main/objc/macos/Trust_MacOS_OBJC/Trust_MacOS_OBJC/OC_TMO_Globals.m
@@ -0,0 +1,13 @@
+//
+//  OC_TMO_Globals.m
+//  Trust_MacOS_OBJC
+//
+//  Created by Presley Cannady on 9/10/17.
+//  Copyright © 2017 Open Correlate. All rights reserved.
+//
+
+#import "OC_TMO_Globals.h"
+
+@implementation OC_TMO_Globals
+
+@end
